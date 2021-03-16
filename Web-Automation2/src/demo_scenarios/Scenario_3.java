942200d4a2bf493d74c79921a20ffb33cfc67e8b
Shilpa132 <chougalashilpa132@gmail.com>
2021-03-16 10:26:16 +0530

    Scenarios 3-7

---
+++ Web-Automation2/src/demo_scenarios/Scenario_3.java
@@ -0,0 +1,121 @@
+package demo_scenarios;
+
+import org.openqa.selenium.Alert;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Scenario_3 {
+	public static WebDriver oBrowser = null;
+	public static void main(String[] args) {
+		
+		launchBrowser();
+		navigate();
+		login();
+        createCustomer();
+		deleteCustomer();
+		logout();
+		closeApplication();
+	}
+	
+	public static void launchBrowser()
+	{
+		try
+		{
+			String path=System.getProperty("user.dir");
+			System.setProperty("webdriver.chrome.driver", path+"\\Library\\drivers\\chromedriver.exe");
+			oBrowser=new ChromeDriver();
+		}catch(Exception e)
+		{
+			e.printStackTrace();
+		}
+	}
+	
+	public static void navigate()
+	{
+		try
+		{
+			oBrowser.get("http://localhost:81/login.do");
+		}catch(Exception e)
+		{
+			e.printStackTrace();
+		}
+	}
+	
+	static void login()
+	{
+		try
+		{
+			oBrowser.findElement(By.id("username")).sendKeys("admin");
+			oBrowser.findElement(By.name("pwd")).sendKeys("manager");
+			oBrowser.findElement(By.xpath("//*[@id='loginButton']/div")).click();
+			Thread.sleep(4000);
+		}catch(Exception e)
+		{
+			e.printStackTrace();
+		}
+	}
+	
+	static void logout()
+	{
+		try
+		{
+			oBrowser.findElement(By.linkText("Logout")).click();
+			Thread.sleep(2000);
+		}catch(Exception e)
+		{
+			e.printStackTrace();
+		}
+	}
+	
+	static void closeApplication()
+	{
+		try
+		{
+			oBrowser.close();
+		}catch(Exception e)
+		{
+			e.printStackTrace();
+		}
+	}
+	
+		
+	static void createCustomer()
+	{
+		try
+		{
+			oBrowser.findElement(By.xpath("//*[@id=\'topnav\']/tbody/tr[1]/td[3]/a")).click();
+			oBrowser.findElement(By.xpath("//div[text()='Add New']")).click();
+			oBrowser.findElement(By.xpath("/html/body/div[12]/div[1]")).click();
+			Thread.sleep(2000);
+			oBrowser.findElement(By.id("customerLightBox_nameField")).sendKeys("Shilpa Chougala");
+			Thread.sleep(2000);
+			oBrowser.findElement(By.xpath("//span[text()='Create Customer']")).click();
+			Thread.sleep(2000);
+		}catch(Exception e)
+		{
+			e.printStackTrace();
+		}
+	}
+	
+	static void deleteCustomer()
+	{
+		try
+		{
+			oBrowser.findElement(By.xpath("//div[@class='editButton available']")).click();
+			Thread.sleep(2000);
+			oBrowser.findElement(By.xpath("//*[@id=\'taskListBlock\']/div[2]/div[1]/div[4]/div/div/div[2]")).click();
+			Thread.sleep(2000);
+			oBrowser.findElement(By.xpath("//*[@id=\'taskListBlock\']/div[2]/div[4]/div/div[3]/div")).click();
+			Thread.sleep(2000);
+			oBrowser.findElement(By.id("customerPanel_deleteConfirm_submitTitle")).click();
+		}catch(Exception e)
+		{
+			e.printStackTrace();
+		}
+	}
+
+}
+
+	
+		
